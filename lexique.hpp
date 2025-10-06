f67697e140f21adca5c75bd74626535bc0f88be5
Philippe OCLOO <ocloophilippe@gmail.com>
2025-10-06 08:26:52 +0200

    init repo

---
+++ lexique.hpp
@@ -0,0 +1,13 @@
+#ifndef LEXIQUE_HPP
+#define LEXIQUE_HPP
+
+#include <string>
+
+class Lexique{
+    private : 
+    string 
+
+    public:
+};
+
+#endif LEXIQUE_HPP
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <string>
+#include "utilitaire.hpp"
+
+int main(){
+
+}
